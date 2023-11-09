14e75651e213284296db7f959fc1e03d050fe56f
unknown <CRIATEC@ijagit10.unijui.intranet>
2023-11-09 10:58:04 -0300

    feat: PessoaController e configuracoes na camada Api

--- aspnet-project/App.Application/Services/PessoaService.cs
+++ aspnet-project/App.Application/Services/PessoaService.cs
@@ -1,13 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using App.Domain.Entities;
+using App.Domain.Interfaces.Application;
+using App.Domain.Interfaces.Repositories;
+using App.Persistence.Repositories;
 
 namespace App.Application.Services
 {
     public class PessoaService : IPessoaService
     {
-        private IRepositoryBase<Pessoa>
+        private IRepositoryBase<Pessoa> _repository { get; set; }
+        public PessoaService(RepositoryBase<Pessoa> repository)
+        {
+            _repository = repository;
+        }
+        private void ValidarDados(Pessoa pessoa)
+        {
+            if (string.IsNullOrEmpty(pessoa.Nome))
+            {
+                throw new ArgumentNullException(nameof(pessoa.Nome), "Nomea não pode estar vazio.");
+            }
+
+            if (string.IsNullOrEmpty(pessoa.Email))
+            {
+                throw new ArgumentNullException(nameof(pessoa.Email), "Email não pode estar vazio.");
+            }
+            if (string.IsNullOrEmpty(pessoa.Senha))
+            {
+                throw new ArgumentNullException(nameof(pessoa.Senha), "Senha não pode estar vazia.");
+            }
+        }
+
+        public void Criar(Pessoa pessoa)
+        {
+            ValidarDados(pessoa);
+
+            _repository.Save(pessoa);
+            _repository.SaveChanges();
+        }
+
+        public void Editar(Pessoa pessoa)
+        {
+            var dadosAntigos = _repository.Query(x => x.Id == pessoa.Id).FirstOrDefault();
+
+            if (dadosAntigos == null)
+            {
+                throw new ArgumentException("Usuário não encontrado.");
+            }
+
+            Pessoa dadosAtualizados new Pessoa();
+            dadosAtualizados.Id = dadosAntigos.Id;
+
+            dadosAtualizados.Nome = (pessoa.Nome != null) ? pessoa.Nome : dadosAntigos.Nome;
+            dadosAtualizados.Email = (pessoa.Email != null) ? pessoa.Email : dadosAntigos.Email;
+            dadosAtualizados.Senha = (pessoa.Senha != null) ? pessoa.Senha : dadosAntigos.Senha;
+
+            _repository.Update(dadosAtualizados);
+            _repository.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var dadosAntigos = _repository.Query(x => x.Id == id).FirstOrDefault();
+
+            if (dadosAntigos == null)
+            {
+                throw new ArgumentException("Usuário não encontrado.");
+            }
+
+            _repository.Delete(id);
+            _repository.Savechanges();
+        }
+
+        public PessoaService BuscarPorId(int id)
+        {
+            var obj = _repository.Query(x => x.Id == id).FirstOrDefault();
+            return obj;
+        }
+
+        public List<Pessoa> BuscarLista()
+        {
+            return _repository.Query(x => 1 == 1).ToList();
+        }
+
     }
 }
