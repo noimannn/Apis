e68433dc212b8a53db894acfa7d372202d0abb7c
Luan Neumann <luan.neumann@abase.com.br>
2023-11-16 10:53:53 -0300

    Realizando um pouco do front com o banco de dados

---
+++ App.Api/App.Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Persistence
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly
+        }
+
+
+    }
+}
